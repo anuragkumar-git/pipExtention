b84228f726856fb85dd230dcc3b20c08427593e3
anuragkumar-git <patelanurag3971@gmail.com>
2024-09-19 14:48:45 +0530

    New feature: Play Pause

--- popup.js
+++ popup.js
@@ -1,42 +1,49 @@
 chrome.action.onClicked.addListener(async (tab) => {
-    // Check if a tab is active
-    if (!tab.id) return;
+    if (!tab.id) return; // Ensure the tab is active
 
     // Inject script into the active tab
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
         func: () => {
-            // Function to trigger PiP for video
-            function activatePiP(video) {
+            // Function to toggle play/pause, PiP, volume, and mute/unmute
+            function controlVideo(video) {
                 if (video) {
-                    video.requestPictureInPicture().catch(err => console.log("Error: " + err));
-                } else {
-                    alert("No video element found on this page.");
-                }
-            }
-
-            // Look for videos in the main document
-            let videos = document.getElementsByTagName("video");
-            if (videos.length > 0) {
-                activatePiP(videos[0]);
-                return;
-            }
-
-            // Look for videos inside iframes
-            const iframes = document.getElementsByTagName('iframe');
-            for (let i = 0; i < iframes.length; i++) {
-                try {
-                    let iframeVideos = iframes[i].contentWindow.document.getElementsByTagName("video");
-                    if (iframeVideos.length > 0) {
-                        activatePiP(iframeVideos[0]);
-                        return;
+                    // Toggle Play/Pause
+                    if (video.paused) {
+                        video.play();
+                        console.log("Video is playing");
+                    } else {
+                        video.pause();
+                        console.log("Video is paused");
                     }
-                } catch (error) {
-                    console.log("Unable to access iframe video due to cross-origin issues.");
-                }
-            }
 
-            alert("No video found in the main document or iframes.");
+                    // Request Picture-in-Picture mode
+                    video.requestPictureInPicture().catch(err => console.log("Error: " + err));
+                }
+
+                // Look for videos in the main document
+                let videos = document.getElementsByTagName("video");
+                if (videos.length > 0) {
+                    controlVideo(videos[0]);
+                    return;
+                }
+
+                // Look for videos inside iframes
+                const iframes = document.getElementsByTagName('iframe');
+                for (let i = 0; i < iframes.length; i++) {
+                    try {
+                        let iframeVideos = iframes[i].contentWindow.document.getElementsByTagName("video");
+                        if (iframeVideos.length > 0) {
+                            controlVideo(iframeVideos[0]);
+                            return;
+                        }
+                    } catch (error) {
+                        console.log("Unable to access iframe video due to cross-origin issues.");
+                    }
+                }
+
+                alert("No video found in the main document or iframes.");
+            }
         }
     });
 });
